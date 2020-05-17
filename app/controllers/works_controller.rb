da77efcfc385472918fc1107ba97cd7c5de59dac
Rosalyn <syrosalynyu@gmail.com>
2020-05-16 17:56:37 -0700

    Created Works routes and Work model

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,31 @@
 class WorksController < ApplicationController
+  def index
+    @works = Work.all
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  
 end
