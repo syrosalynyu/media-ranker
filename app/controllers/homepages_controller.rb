e47028f684c02bcb93e9350d11859a1ade9c6314
Rosalyn <syrosalynyu@gmail.com>
2020-05-17 12:30:23 -0700

    Finished the layout of homepage & root is now linked to the homepage

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,2 +1,8 @@
 class HomepagesController < ApplicationController
+  def index
+    @works = Work.all
+    @albums = Work.where(category: "album")
+    @books = Work.where(category: "book")
+    @movies = Work.where(category: "movie")
+  end
 end
