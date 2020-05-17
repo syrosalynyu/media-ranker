b3a10721c5e2e07a45104978da1b31cd0e321177
Rosalyn <syrosalynyu@gmail.com>
2020-05-16 22:59:16 -0700

    Completed Works Controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,28 +4,69 @@ class WorksController < ApplicationController
   end
 
   def new
-
+    @work = Work.new
   end
 
   def create
-
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :new
+      return
+    end
   end
 
   def show
+    @work = Work.find_by(id: params[:id])
 
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def edit
+    @work = Work.find_by(id: params[:id])
 
+    if @work.nil?
+      head :not_found
+      return
+    end
   end
 
   def update
+    @work = Work.find_by(id: params[:id])
 
+    if @work.nil?
+      head :not_found
+      return
+    elsif @work.update(work_params)
+      redirect_to work_path(@work.id)
+      return
+    else
+      render :edit
+      return
+    end
   end
 
   def destroy
+    @work = Work.find_by(id: params[:id])
 
+    if @work.nil?
+      head :not_found
+      return
+    else
+      @work.destroy
+      redirect_to works_path
+      return
+    end
   end
 
-  
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :year, :description)
+  end 
 end
