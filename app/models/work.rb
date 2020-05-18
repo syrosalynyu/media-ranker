076939e6db54a527f9778d1f120cff2e3dfd899f
Rosalyn <syrosalynyu@gmail.com>
2020-05-17 18:19:46 -0700

    implemented validations and completed validation tests for Works

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,5 @@
 class Work < ApplicationRecord
+  validates :category, inclusion: { in: %w(album book movie) }
+  # TODO: how to change it so that each category can have the same name once?
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
