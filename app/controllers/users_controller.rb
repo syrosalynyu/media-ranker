a24ae8ddff222c6b4a536dbfc3b13c9d41946a43
Rosalyn <syrosalynyu@gmail.com>
2020-05-20 16:19:46 -0700

    completed user lists and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
   def login_form
     @user = User.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
 
+  resources :users, only: [:index, :show]
   get "/login", to: "users#login_form", as: "login"
   post "/login/", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
