ccccf101856dda1fa6be16d0945d9fcc1e961bd5
Rosalyn <syrosalynyu@gmail.com>
2020-05-21 22:01:58 -0700

    relationship tests for User and Vote model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -35,4 +35,22 @@ describe User do
       expect(@user2.errors.messages).must_include :username
     end
   end
+
+  describe "has_many relationship" do
+    before do
+      # Arrange
+      @work = Work.create!(title: "test work", category: "album", creator: "test creator", year: 1990, description: "This is the description for the test work")
+      @user = User.create!(username: "loren",
+        joined: Date.today)
+      @vote = Vote.create!(work_id: @work.id, user_id: @user.id)
+    end
+
+    it "user has many votes" do 
+      expect(@user.votes).must_include @vote
+    end
+
+    it "user has many works" do
+      expect(@user.works).must_include @work
+    end
+  end
 end
