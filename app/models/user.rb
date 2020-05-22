a46097a7eb1a948fb5dd2df87a7cec3f4cf6bf6e
Rosalyn <syrosalynyu@gmail.com>
2020-05-22 14:18:56 -0700

    add dependent destroy to User and Work model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }
 
-  has_many :votes
+  has_many :votes, dependent: :destroy 
   has_many :works, through: :votes
 end
